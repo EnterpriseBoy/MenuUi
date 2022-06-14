1c32deea4679b45a4719620ae088a85d70be3713
Niall Maguire <nmaguire@boylesports.com>
2022-06-14 14:35:18 +0100

    Header added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import { Routes, Route } from "react-router-dom"
+
+//Components
 import Home from "./Components/Home/Home"
 import About from "./Components/About/About"
 import Contact from "./Components/Contact/Contact"
+import Header from "./Components/Header/Header"
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <Routes>
         <Route path="/" element={ <Home/> } />
         <Route path="about" element={ <About/> } />
