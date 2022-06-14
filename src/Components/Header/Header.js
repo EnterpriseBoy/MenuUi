db4454eb2be622fc8c065bd361fc814d78cac71b
Niall Maguire <nmaguire@boylesports.com>
2022-06-14 14:47:38 +0100

    Font awesome setup

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,8 +1,14 @@
 import React from 'react'
 
+//FontAwesome
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+
+
 function Header() {
     return (
       <div>
+        <FontAwesomeIcon icon={faCoffee} />
         <h1>This is the header</h1>
       </div>
     );
